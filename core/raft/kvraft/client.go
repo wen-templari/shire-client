5621df204b2e0c5632c6fbf5ee8276f139e929af
templari <q79142466@163.com>
2023-05-16 21:19:39 +0800

    save

--- core/raft/kvraft/client.go
+++ core/raft/kvraft/client.go
@@ -114,6 +114,9 @@ func (ck *Clerk) PutAppend(key string, value string, op string) {
 		for i := nextStart; i < len(ck.servers); i++ {
 			// log.Println(ck.servers[i])
 			reply := PutAppendReply{}
+			if ck.servers[i] == nil {
+				continue
+			}
 			if err := ck.servers[i].Call("KVServer.PutAppend", &args, &reply); err != nil {
 				// ck.mu.Lock()
 				// ck.setRecentLeader(0)
